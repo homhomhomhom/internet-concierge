775b22b73400510c2f3ec253e1992093eb8908ea
Gio <531406@student.glu.nl>
2020-03-10 09:36:05 +0100

    added randomxp function

--- bot.js
+++ bot.js
@@ -141,6 +141,10 @@ bot.on('guildMemberRemove', member =>{
 })
 
 //level script
+
+function randomXP() {
+  return Math.floor(Math.random() * 7) + 8;
+}
 bot.on('message', message => {
   if (message.author.bot) return;
 
