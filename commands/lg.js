2b338cc624a7364bd271728e35bf3ebd111a99f5
Gio <531406@student.glu.nl>
2020-03-11 22:26:36 +0100

    tweaked some of the commands

--- commands/lg.js
+++ commands/lg.js
@@ -7,6 +7,13 @@ module.exports.run = async(bot, message, args, con)=>{
         if(e) throw(e)
         if(r.length === 0){
             message.channel.send('Je ziet nog niet in een groep')
+        }else{
+            con.query(`DELETE FROM grouped WHERE member_id = '${message.author.id}' AND group_name = '${group_name}'`, (e)=>{
+                if(e) throw e
+
+                console.log(`${message.author.username} left ${group_name}`)
+                message.channel.send(`Je bent **${group_name}** successvol verlaten`)
+            }) 
         }
     })
 }
