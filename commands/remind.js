2a05f64ae041011c4b28803df3473f4d8aa5883a
Gio <531406@student.glu.nl>
2020-03-17 19:53:31 +0100

    added more logging

--- commands/remind.js
+++ commands/remind.js
@@ -2,14 +2,15 @@ const ms = require('ms')
 
 module.exports.run = async(bot, message, args) =>{
     let messageArray = message.content.split(" ")
-    let announcement = args[0]
+    let reminder = args[0]
     let remindtime = args[1]
     if(!remindtime) return message.reply('Wanneer wil je herinnert worden?')
     
-    message.reply(`Ik zal je over **${remindtime}** herinneren aan: **${announcement}**`)
-
+    message.reply(`Ik zal je over **${remindtime}** herinneren aan: **${reminder}**`)
+    console.log(`${message.author.username} set a reminder called: ${reminder} and it will be executed in ${remindtime}`)
     setTimeout(()=>{
-        message.author.send(`Je herinnering van **${remindtime}** geleden: **${announcement}**`)
+        message.author.send(`Je herinnering van **${remindtime}** geleden: **${reminder}**`)
+        console.log(`I just send ${message.author.username} a reminder for ${reminder}`)
     }, ms(remindtime))
 }
 
