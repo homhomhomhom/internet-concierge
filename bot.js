713801ffd2c7f393606336c005ea73b93008d695
Gio <531406@student.glu.nl>
2020-03-17 20:04:18 +0100

    updated messages

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers`);
-  bot.user.setActivity("over het GLU uwu", {
+  bot.user.setActivity("robots kunnen geen corona krijgen 🤓", {
     type: "WATCHING"
   });
 });
